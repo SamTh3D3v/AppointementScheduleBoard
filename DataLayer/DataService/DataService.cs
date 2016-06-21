e353381536d78f18231592ad36291f645ec5ff38
MouradElmain <mourad.elmain@outlook.com>
2016-06-21 20:45:08 +0100

    clean data service

--- DataLayer/DataService/DataService.cs
+++ DataLayer/DataService/DataService.cs
@@ -7,44 +7,6 @@ using DataLayer.Model;
 
 using Oracle.DataAccess.Client;
 
-class ConnectionSample
-{
-    static void sample()
-    {
-        OracleConnection con = new OracleConnection();
-
-        //using connection string attributes to connect to Oracle Database
-        con.ConnectionString = "user id=PPDWHSIN;password=oracle4U;data source=" +
-                                "(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)" +
-                                 "(HOST=192.168.70.240)(PORT=1521))(CONNECT_DATA=" +
-                                  "(SERVICE_NAME=orcl)))";
-
-        OracleCommand query = new OracleCommand("select sysdate from dual", con);
-        con.Open();
-        OracleDataReader reader = query.ExecuteReader();
-        if (reader.HasRows)
-        {
-            while (reader.Read())
-            {
-                Console.WriteLine("{0}", reader.GetOracleDate(0));
-            }
-        }
-        else
-        {
-            Console.WriteLine("No rows found.");
-        }
-        reader.Close();
-
-        Console.WriteLine("Connected to Oracle" + con.ServerVersion);
-        Console.ReadLine();
-        // Close and Dispose OracleConnection object
-        con.Close();
-        con.Dispose();
-        Console.WriteLine("Disconnected");
-    }
-}
-
-
 namespace DataLayer.DataService
 {
     public class DataService: IDataService
